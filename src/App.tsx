5c2c6b83022dee60b0a8083190cf6a9ad3ff62d5
Lasse Hay-Schmidt <36488481+haysch@users.noreply.github.com>
2024-09-03 09:12:10 +0200

    Align page a bit better

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
     );
   }
   return (
-    <div className="container">
-      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <div className="container py-3">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between pb-3 mb-3 border-bottom">
         <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
           Criipto Signatures Demo
         </a>
